406d5c7152500a1e6214ee8364bd4126d708990f
Mojtaba Ghasemi <mghasemi13786@gmail.com>
2022-04-21 14:00:08 +0430

    add paginator feature

--- network/static/network/script.js
+++ network/static/network/script.js
@@ -1,19 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     // َAdd new post
     document.querySelector("#new-post-form").onsubmit = add_new_post;
-
-    // // Infinite scroll
-    // window.onscroll = function () {
-    //     if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
-    //         post_index += 10;
-    //         load_post();
-    //     }
-    // }
-
-    load_post();
 });
-
-// let post_index = 0;
 
 function add_new_post() {
     const post_content = document.querySelector("#new-post-content");
@@ -28,8 +16,8 @@ function add_new_post() {
             .then(response => response.json())
             .then(data => {
                 post_content.value = '';
-                load_post();
                 alert(data.success);
+                location.reload();
             });
 
     } else {
@@ -38,30 +26,30 @@ function add_new_post() {
     return false;
 }
 
-function load_post() {
-    let type;
-    if (window.location.pathname === "/following-post")
-        type = "following";
-    else
-        type = "all";
-
-    const all_post = document.querySelector("#all-posts");
-
-    fetch(`/load_post/${type}`, {
-        method: "Get"
-    }).then(response => response.json())
-        .then(data => {
-            data.forEach(post => {
-                let node = document.createElement("div");
-                node.className = "post border m-3 p-3";
-                node.innerHTML = `<div><h5><a href="/user/${post.owner}">${post.owner}</a> <small>Follow</small></h5><small style="float: right;">${post.create_date}</small> </div>
-                                <p>${post.text}</p>
-                                <i class="bi bi-heart" title="like"></i>
-                                <small>Likes ${post.likes}</small>`;
-
-                all_post.append(node);
-            });
-        });
-
-
-}
+// function load_post() {
+//     let type;
+//     if (window.location.pathname === "/following-post")
+//         type = "following";
+//     else
+//         type = "all";
+//
+//     const all_post = document.querySelector("#all-posts");
+//
+//     fetch(`/load_post/${type}`, {
+//         method: "Get"
+//     }).then(response => response.json())
+//         .then(data => {
+//             data.forEach(post => {
+//                 let node = document.createElement("div");
+//                 node.className = "post border m-3 p-3";
+//                 node.innerHTML = `<div><h5><a href="/user/${post.owner}">${post.owner}</a> <small>Follow</small></h5><small style="float: right;">${post.create_date}</small> </div>
+//                                 <p>${post.text}</p>
+//                                 <i class="bi bi-heart" title="like"></i>
+//                                 <small>Likes ${post.likes}</small>`;
+//
+//                 all_post.append(node);
+//             });
+//         });
+//
+//
+// }
